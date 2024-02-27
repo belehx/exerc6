d918e641e0763716942f73a90c2f7cb4177559b7
belehx <161518280+belehx@users.noreply.github.com>
2024-02-27 20:39:21 -0300

    Add files via upload

---
+++ exerc6/exerc6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerc6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1;
+            Console.WriteLine("Digite um número: ");
+
+            a1= int.Parse(Console.ReadLine());
+
+            if (a1 % 4 == 0)
+            {
+                Console.WriteLine("Número válido");
+            }
+            else
+            {
+                Console.WriteLine("Número Inválido");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
